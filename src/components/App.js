dc32c77aced0b091fe5fd631f00971f83ac82fc8
Hazrat Ali <hazrat8143@gmail.com>
2024-04-23 00:02:33 +0530

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,24 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React, { useState } from "react";
+import Todos from "./Todos";
+import Memo from "./Memo";
+import Count from "./Count";
+import { useEffect } from "react";
 
+let App = () => {
+  useEffect(() => {}, [count, memo, todos]);
+  let [todos, Settodos] = useState(["New Todo"]);
+  let [memo, Setmemo] = useState(["HTMLL", "CSSS3"]);
+  let [count, Setcount] = useState(1000000000);
+  return (
+    <div id="main">
+      <h1>React.useMEMo</h1>
+      <Todos set={Settodos} arr={todos} />
+      <Count set={Setcount} number={count} />
+      <h1>Expensive Calculation</h1>
+      {/* <div id='calc'>1000000000</div> */}
+      <Memo set={Setmemo} arr={memo} />
+    </div>
+  );
+};
+
+export default App;
